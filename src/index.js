c9ef9865176462aef250862074ed4a75fc88a3a5
Yuliia <123005661+Yuliia-Diachenko@users.noreply.github.com>
2024-10-22 21:45:18 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import  setupServer  from "./server.js";
+import { initMongoConnection } from "./db/initMongoConnection.js";
+
+const bootstrap = async () => {
+    try {
+await initMongoConnection();
+setupServer();
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+bootstrap();
